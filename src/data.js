4bc13db77e4fa7353a10402b50a37440cdef7e6c
Varuna Tandon <varunatandon82@gmail.com>
2021-10-20 19:53:27 +0530

    Added gh pages

---
+++ src/data.js
@@ -0,0 +1,23 @@
+const data = {
+    products : [
+        {
+            id : 'serram16',
+            name : '16GB RAM SERVER',
+            price : 1025.50,
+            image : 'https://via.placeholder.com/350x150'
+        },
+        {
+            id : 'serram32',
+            name : '32GB RAM SERVER',
+            price : 2549.99,
+            image : 'https://via.placeholder.com/350x150'
+        },
+        {
+            id : 'serram8',
+            name : '8GB RAM SERVER',
+            price : 100,
+            image : 'https://via.placeholder.com/350x150'
+        }
+    ]
+}
+export default data;
